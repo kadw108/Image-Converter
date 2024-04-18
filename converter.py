e27cefb590ab7cac2c32999b2c33fe599f0e2cfe
kadw108 <wallwritinglast@gmail.com>
2024-04-18 14:07:51 -0400

    Comment code, add more functions + examples
    Actually kind of usable by other people now.

--- converter.py
+++ converter.py
@@ -12,40 +12,35 @@ import math
 from glitch_this import ImageGlitcher
 glitcher = ImageGlitcher()
 
-# input_path = "/home/account/Documents/writ/if/shufflecomp/crumbling_castle/crumbling_castle_source/export/assets/image_archive/conversion"
+import re
+
 current_path = os.path.abspath(os.getcwd())
 input_path = current_path
 
-def rename(i, output_num, input_num):
-    if str(input_num) != "-1" and not os.path.basename(i).startswith(str(input_num)):
-        return
+""" IMAGE OR GIF ALTERING FUNCTIONS """
 
-    inp = Image.open(i)
-    inp.save(filename(i, output_num))
+def posterize(filename, output_name):
+    """
+    Posterize input png.
+    """
 
-def filename(i, num):
-    i = os.path.basename(i)
-    return str(num) + i[1:-4] + ".png"
-
-def posterize(i, output_num, input_num):
-    if not os.path.basename(i).startswith(str(input_num)):
-        return
-
-    inp = Image.open(i)
+    inp = Image.open(filename)
     inp = ImageOps.posterize(inp, 3)
-    inp.save(filename(i, output_num))
+    inp.save(output_name)
 
-def dither(i, output_num, input_num, keep_transparency = True):
-    if not os.path.basename(i).startswith(str(input_num)):
-        return
+def dither(filename, output_name, keep_transparency = True):
+    """
+    Dither input png.
 
-    inp = cv2.imread(i)
+    keep_transparency -- whether to preserve alpha in input png
+    """
+
+    inp = cv2.imread(filename)
     inp = ordered_dither(inp, "Bayer2x2")
-    output_name = filename(i, output_num)
     cv2.imwrite(output_name, inp)
 
     if keep_transparency:
-        base_img = Image.open(i).convert("RGBA")
+        base_img = Image.open(filename).convert("RGBA")
         overlay_img = Image.open(output_name).convert("RGBA")
         alpha = base_img.split()[-1]
         mask_image = Image.new("RGBA", base_img.size, (0, 0, 0, 255))
@@ -54,15 +49,15 @@ def dither(i, output_num, input_num, keep_transparency = True):
 
         overlay_img.putalpha(mask_image)
         overlay_img.save(output_name)
-        return
-        real_output = Image.composite(base_img, overlay_img, mask_image)
-        real_output.save(output_name)
 
-def resize(i, output_num, input_num, min_height = 804, max_height = 850):
-    if not os.path.basename(i).startswith(str(input_num)):
-        return
+        # real_output = Image.composite(base_img, overlay_img, mask_image)
+        # real_output.save(output_name)
 
-    inp = Image.open(i)
+def resize(filename, output_name, min_height = 804, max_height = 850):
+    """
+    Resize input png.
+    """
+    inp = Image.open(filename)
     if (inp.height < min_height):
         factor = min_height/inp.height
         inp = inp.resize((math.ceil(inp.width * factor), math.ceil(inp.height * factor)), resample=Image.Resampling.BILINEAR)
@@ -71,23 +66,137 @@ def resize(i, output_num, input_num, min_height = 804, max_height = 850):
         factor = max_height/inp.height
         inp = inp.resize((math.ceil(inp.width * factor), math.ceil(inp.height * factor)), resample=Image.Resampling.NEAREST)
 
-    inp.save(filename(i, output_num))
+    inp.save(output_name)
 
 # pixelate
-def pypxl(i, output_num, input_num):
-    if not os.path.basename(i).startswith(str(input_num)):
-        return
-
+def pypxl(filename, output_name):
+    """
+    Apply pixelate effect to input png.
+    """
     input_path2 = os.path.join(current_path, "pypxl")
     os.chdir(input_path2)
 
-    command = "python3 pypxl_image.py -s 512 512 " + os.path.join(input_path, i) + " " + os.path.join(input_path, filename(i, output_num))
+    command = "python3 pypxl_image.py -s 512 512 " + os.path.join(input_path, filename) + " " + os.path.join(input_path, output_name)
     os.system(command)
 
     os.chdir(current_path)
 
-# easier to use a screenshot with https://coolors.co/gradient-maker
+def apply_colormap(filename, output_name, colormap_name, gradient_alpha):
+    """
+    Applies gradient map/colormap to input png.
+
+    colormap_name -- path to colormap, 1d horizontal pixel strip with lighter colors to the right
+    gradient_alpha -- strength of colormap; number from [0, 255] with higher = stronger colormap
+    """
+    command = "convert " + filename + \
+        " -channel RGB -separate -evaluate-sequence mean " +  \
+        colormap_name + \
+        " -clut " +  \
+        "temp.png"
+    os.system(command)
+
+    base_img = Image.open(filename)
+    overlay_img = Image.open("temp.png").convert("RGBA")
+    overlay_img.putalpha(gradient_alpha)
+    base_img.paste(overlay_img, mask=overlay_img)
+    base_img.save(output_name)
+
+def glitch(filename, output_name):
+    """
+    Turns input png into glitchy gif.
+    """
+    # glitch_image(self, src_img, glitch_amount, glitch_change=0.0, cycle=False, color_offset=False, scan_lines=False, gif=False, frames=23, step=1)
+    glitch_img = glitcher.glitch_image(filename, 0.7, color_offset = True, gif=True)
+    # glitch_img = glitcher.glitch_image(i, 0.5, scan_lines = True, gif=True)
+
+    DURATION = 200      # Set this to however many centiseconds each frame should be visible for
+    LOOP = 0            # Set this to how many times the gif should loop
+                        # LOOP = 0 means infinite loop
+    glitch_img[0].save(output_name + ".gif",
+                   format='GIF',
+                   append_images=glitch_img[1:],
+                   save_all=True,
+                   duration=DURATION,
+                   loop=LOOP)
+
+def downsize_gif(filename, output_name, percent):
+    """
+    Reduces actual size of input gif, which also reduces file size.
+    Requires convert to work on command line.
+
+    percent -- [0-100], percentage of original size to keep.
+    """
+
+    command = "convert -resize " + str(percent) + "% " + filename + " " + output_name
+    os.system(command)
+
+def optimize_gif(filename, output_name, lossy = 30, color_num = None):
+    """
+    Optimize input gif to reduce file size. 
+    Requires gifsicle to work on command line.
+
+    color_num -- colorspace/number of colors in resulting gif, suggested values from 16 to 256. Optional. Number of colors will not be reduced if not included.
+    lossy -- level of compression, suggested values from 30 to 200, with higher = more compression.
+    """
+   
+    color_num_arg = ""
+    if isinstance(color_num, int):
+        color_num_arg = "--colors " + str(color_num)
+
+    command = "gifsicle -O3 " + color_num_arg + " --lossy=" + str(lossy) + " -o " + output_name + " " + filename
+    os.system(command)
+
+def change_gif_speed(filename, output_name, fps = 12):
+    """
+    Change speed of input gif to fps.
+    Requires convert to work on command line.
+    """
+    argument = str(int(100/fps)) # Default fps of 12 makes this 8
+
+    command = "convert -delay " + argument + " " + os.path.join(input_path, filename) + " " + os.path.join(input_path, output_name)
+    os.system(command)
+
+def reduce_gif_frames(filename, output_name, skip_frames=2):
+    """
+    Removes all frames except every [skip_frames] frame.
+
+    i.e. skip_frames=2 keeps frames 0, 2, 4... skip_frames=4 keeps frames 0, 4, 8...
+    """
+
+    im = Image.open(filename)
+    num_frames = str(im.n_frames - 1) # frames are 0-indexed in gifsicle
+
+    command = "gifsicle -U " + filename + " `seq -f \"#%g\" 0 " + str(skip_frames) + " " + num_frames + "` -o " + output_name
+    os.system(command)
+
+""" UTILITY FUNCTIONS """
+
+def rename(filename, output_name):
+    """
+    Opens filename as image and saves it under output_name.
+    Does not delete original file.
+    """
+    inp = Image.open(filename)
+    inp.save(output_name)
+
+def get_output_filename(filename, output_num):
+    """
+    If filename starts with a number, replaces that num with output_num.
+    Returns the new string.
+    """
+
+    filename = os.path.basename(filename)
+
+    number_match = re.search(r'\d+', filename)
+    if not number_match:
+        return str(output_num) + filename
+
+    return str(output_num) + filename[len(number_match.group()):]
+
 def gen_colormaps():
+    """
+    Use not recommended - easier to use a screenshot with https://coolors.co/gradient-maker
+    """
     colormap_name = "colormap.png"
 
     colors = ["rgb(0, 50, 100)", "rgb(220, 110, 110)", "rgb(255, 255, 0)", "rgb(255, 255, 255)"]
@@ -103,55 +212,51 @@ def gen_colormaps():
 
     return colormap_name
 
-def apply_colormap(i, output_num, input_num, colormap_name, gradient_alpha):
-    if not os.path.basename(i).startswith(str(input_num)):
+def callback_on_one(callback, filename, input_num, output_num, *args, **kwargs):
+    """
+    Executes callback(filename, output_name, kwargs) only if filename begins with input_num.
+    output_name will be filename, but beginning with output_num instead of input_num.
+    """
+    if not os.path.basename(filename).startswith(str(input_num)):
         return
+    output_name = get_output_filename(filename, output_num)
+    callback(filename, output_name, *args, **kwargs)
 
-    command = "convert " + i + \
-        " -channel RGB -separate -evaluate-sequence mean " +  \
-        colormap_name + \
-        " -clut " +  \
-        "temp.png"
-    os.system(command)
+def callback_all(callback, input_num, output_num, *args, **kwargs):
+    """
+    For all files in input_path starting with input_num, executes callback on them,
+    saving callback's output to a filename beginning with output_num instead of input_num.
 
-    base_img = Image.open(i)
-    overlay_img = Image.open("temp.png").convert("RGBA")
-    overlay_img.putalpha(gradient_alpha)
-    base_img.paste(overlay_img, mask=overlay_img)
-    base_img.save(filename(i, output_num))
+    Meant to be used as a wrapper around the other image manipulation functions.
+    Since it lets you operate on image files in bulk.
+    """
+    print("--- Callback:", callback.__name__, "| Input num:", input_num, "| Output num:", output_num, "---\n")
+    gather_files = [x for x in sorted(os.listdir(input_path)) if (x.endswith(".png") or x.endswith(".gif"))]
+    for filename in gather_files:
+        full_filename = os.path.join(input_path, filename)
+        callback_on_one(callback, full_filename, input_num, output_num, *args, **kwargs)
 
-def glitch(i, output_num, input_num):
-    if not os.path.basename(i).startswith(str(input_num)):
-        return
-
-    # glitch_image(self, src_img, glitch_amount, glitch_change=0.0, cycle=False, color_offset=False, scan_lines=False, gif=False, frames=23, step=1)
-    glitch_img = glitcher.glitch_image(i, 0.7, color_offset = True, gif=True)
-    # glitch_img = glitcher.glitch_image(i, 0.5, scan_lines = True, gif=True)
-
-    DURATION = 200      # Set this to however many centiseconds each frame should be visible for
-    LOOP = 0            # Set this to how many times the gif should loop
-                        # LOOP = 0 means infinite loop
-    glitch_img[0].save(filename(i, output_num) + ".gif",
-                   format='GIF',
-                   append_images=glitch_img[1:],
-                   save_all=True,
-                   duration=DURATION,
-                   loop=LOOP)
+""" MAIN """
 
 if __name__ == "__main__":
-    # name = gen_colormaps()
-    for i in [x for x in sorted(os.listdir(input_path)) if x.endswith(".png")]:
-        print(i)
-        i = os.path.join(input_path, i)
-        # rename(i, 1, 0)
-        # pypxl(i, 1, 0)
-        # resize(i, 2, 1, max_height = 350)
-        # dither(i, 2, 1) # don't use?
-        # apply_colormap(i, 3, 2, "colormap_greenhouse.png", 190)
-        # resize(i, 4, 3, min_height = 850)
-        glitch(i, 5, 4)
-        pass
 
-    # i = "1dorm.png"
-    # pypxl(i, 1, 0)
-    # glitch(i, 1, 1)
+    # Example of using the image functions in bulk.
+    # This will take a bunch of images starting with 0 and ending with .png and turn them into cool looking glitchy gifs (assuming you have the right libraries and command line functions working)
+    # You can comment in/out specific parts of the pipeline if you want to swap out certain operations for other ones, or if you like the product of a certain step but not what happens after that.
+    # Furthermore, since different input/output numbers are used for each step, you can check the results of each operation.
+
+    callback_all(pypxl, 0, 1)
+    callback_all(resize, 1, 2, max_height = 350)
+    callback_all(apply_colormap, 2, 3, colormap_name = "colormap_greenhouse.png", gradient_alpha = 150)
+    callback_all(glitch, 3, 4)
+    callback_all(change_gif_speed, 4, 5, fps = 6)
+    callback_all(reduce_gif_frames, 5, 6)
+    callback_all(optimize_gif, 6, 7, lossy=200, color_num = 16)
+
+    """
+    # Example of using the image functions on 1 image.
+
+    i = "0dorm.png"
+    callback_on_one(pypxl, i, 0, 1)
+    callback_on_one(glitch, i, 1, 1)
+    """
